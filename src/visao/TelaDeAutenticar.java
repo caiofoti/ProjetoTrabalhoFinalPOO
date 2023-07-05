27960508be27696ac4f99957c5776fc30ce80866
Victor Octávio <victoroctavio4@gmail.com>
2023-07-05 19:48:20 -0300

    Att imagens e Cort e Inc finalizadas

--- src/visao/TelaDeAutenticar.java
+++ src/visao/TelaDeAutenticar.java
@@ -9,6 +9,9 @@ import java.awt.Font;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+import java.awt.Color;
 
 public class TelaDeAutenticar extends JPanel {
 	
@@ -20,17 +23,29 @@ public class TelaDeAutenticar extends JPanel {
 	private JPasswordField fieldSenha;
 	private JButton buttonAutenticar;
 	private JButton buttonCancelar;
+	private JLabel imagemPedidosInternos;
+	private JLabel imagemLogin;
 
 	/**
 	 * Create the panel.
 	 */
 	public TelaDeAutenticar() {
+		setBackground(new Color(255, 255, 255));
 		setBounds(100, 100, 665, 658);
-		setLayout(new MigLayout("", "[grow]", "[grow]"));
+		setLayout(new MigLayout("", "[3.00][374.00][333.00]", "[]"));
 		
 		JPanel panel = new JPanel();
-		add(panel, "cell 0 0,grow");
-		panel.setLayout(new MigLayout("", "[20.00][585.00,grow][grow]", "[][][][86.00][][20.00][13.00][22.00][16.00][26.00][][][][][][][][][][]"));
+		panel.setBackground(SystemColor.activeCaption);
+		add(panel, "cell 1 0 2 1,grow");
+		panel.setLayout(new MigLayout("", "[297.00][239.00,grow][][-37.00,grow]", "[][][][30px][][20.00][13.00][22.00][16.00][26.00][][][][][][][][][][]"));
+		
+		imagemLogin = new JLabel("");
+		imagemLogin.setIcon(new ImageIcon(TelaDeAutenticar.class.getResource("/figuras/asdfasfdasfdasdfafdf.jpg")));
+		panel.add(imagemLogin, "cell 0 1 1 18");
+		
+		imagemPedidosInternos = new JLabel("");
+		imagemPedidosInternos.setIcon(new ImageIcon(TelaDeAutenticar.class.getResource("/figuras/Icon_PedidosInternos.png")));
+		panel.add(imagemPedidosInternos, "cell 1 2,alignx center");
 		
 		JLabel labelUsuario = new JLabel("Usuário");
 		labelUsuario.setFont(new Font("Arial", Font.BOLD, 11));
--- src/visao/TelaIncendio.java
+++ src/visao/TelaIncendio.java
@@ -48,74 +48,74 @@ public class TelaIncendio extends JPanel {
 		
 		JPanel panel = new JPanel();
 		scrollPane.setViewportView(panel);
-		panel.setLayout(new MigLayout("", "[636.00,grow]", "[][][][][][][][][][][][][][80px][][][][][][][][][][][][][][80px,grow][][]"));
+		panel.setLayout(new MigLayout("", "[636.00,grow]", "[][][][][][][][][][][][][80px][][][][][][][][][][][][][][80px,grow][][]"));
 		
-		JLabel labelFoto = new JLabel("");
-		labelFoto.setIcon(new ImageIcon("ProjetoTrabalhoFinalPOO\\src\\TelaPedidos4.png"));
-		panel.add(labelFoto, "cell 0 0,grow");
+		JLabel imagemPedidosInternos = new JLabel("");
+		imagemPedidosInternos.setIcon(new ImageIcon(TelaIncendio.class.getResource("/figuras/TelaPedidos4.png")));
+		panel.add(imagemPedidosInternos, "cell 0 0");
 		
 		JLabel labelServico = new JLabel("Serviço:");
 		labelServico.setFont(new Font("Tahoma", Font.BOLD, 11));
-		panel.add(labelServico, "cell 0 2");
+		panel.add(labelServico, "cell 0 1");
 		
 		JLabel labelSetorEncarregado = new JLabel("Setor encarregado: ");
 		labelSetorEncarregado.setFont(new Font("Arial", Font.BOLD, 11));
-		panel.add(labelSetorEncarregado, "cell 0 3");
+		panel.add(labelSetorEncarregado, "cell 0 2");
 		
 		JLabel labelSolic = new JLabel("Quem pode solicitar:");
 		labelSolic.setFont(new Font("Tahoma", Font.BOLD, 11));
-		panel.add(labelSolic, "cell 0 4");
+		panel.add(labelSolic, "cell 0 3");
 		
 		JLabel labelAtende = new JLabel("Geralmente atende em:");
 		labelAtende.setFont(new Font("Arial", Font.BOLD, 11));
-		panel.add(labelAtende, "cell 0 5");
+		panel.add(labelAtende, "cell 0 4");
 		
 		JLabel labelTexto1 = new JLabel("<html>O prazo para atendimento será contado da data de disponibilização do formulário à empresa contratada de acordo com sua classificação: chamado urgente (2 horas em dias úteis ou 4 horas nos demais dias não úteis) chamado normal (48 horas em dias úteis ou 72 horas nos demais dias não úteis.</html>");
 		labelTexto1.setFont(new Font("Arial", Font.PLAIN, 11));
-		panel.add(labelTexto1, "cell 0 6");
+		panel.add(labelTexto1, "cell 0 5");
 		
 		JLabel labelTexto2 = new JLabel("<html>Caso o preenchimento da ficha de atendimento seja realizado posterior ao horário máximo permitido acima, poderá ser aplicado o instrumento de medição de resultados previsto no anexo A do Termo de Referência (TR), bem como as sanções previstas no item 19 do TR.</html>");
 		labelTexto2.setFont(new Font("Arial", Font.PLAIN, 11));
-		panel.add(labelTexto2, "cell 0 7");
+		panel.add(labelTexto2, "cell 0 6");
 		
 		Label labelDados = new Label("Dados de Solicitação");
 		labelDados.setForeground(Color.WHITE);
 		labelDados.setFont(new Font("Arial Black", Font.BOLD, 16));
 		labelDados.setBackground(new Color(51, 51, 102));
 		labelDados.setAlignment(Label.CENTER);
-		panel.add(labelDados, "cell 0 9,grow");
+		panel.add(labelDados, "cell 0 8,grow");
 		
 		JLabel labelIrregularidade = new JLabel("Descreva a irregularidade");
 		labelIrregularidade.setFont(new Font("Arial", Font.BOLD, 14));
-		panel.add(labelIrregularidade, "flowx,cell 0 11");
+		panel.add(labelIrregularidade, "flowx,cell 0 10");
 		
 		JLabel labelDetalhesIrregularidade = new JLabel("Inserir com o máximo de detalhes possível, qual a irregularidade identificada");
 		labelDetalhesIrregularidade.setFont(new Font("Arial", Font.PLAIN, 11));
-		panel.add(labelDetalhesIrregularidade, "cell 0 12");
+		panel.add(labelDetalhesIrregularidade, "cell 0 11");
 		
 		irregArea = new JTextArea();
 		irregArea.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 2, true), "Descri\u00E7\u00E3o", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.add(irregArea, "cell 0 13,grow");
+		panel.add(irregArea, "cell 0 12,grow");
 		
 		JLabel labelAsterisco = new JLabel("*");
 		labelAsterisco.setForeground(Color.RED);
 		labelAsterisco.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		panel.add(labelAsterisco, "cell 0 11");
+		panel.add(labelAsterisco, "cell 0 10");
 		
 		JLabel LabelSist = new JLabel("Sistema");
 		LabelSist.setFont(new Font("Arial", Font.BOLD, 14));
-		panel.add(LabelSist, "flowx,cell 0 15");
+		panel.add(LabelSist, "flowx,cell 0 14");
 		
 		JLabel labelAsterisco1 = new JLabel("*");
 		labelAsterisco1.setForeground(Color.RED);
 		labelAsterisco1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		panel.add(labelAsterisco1, "cell 0 15");
+		panel.add(labelAsterisco1, "cell 0 14");
 		
 		comboSist = new JComboBox<String>();
 		comboSist.setForeground(Color.DARK_GRAY);
 		comboSist.setFont(new Font("Arial", Font.PLAIN, 11));
 		comboSist.setBackground(Color.WHITE);
-		panel.add(comboSist, "cell 0 16,alignx left");
+		panel.add(comboSist, "cell 0 15,alignx left");
 		comboSist.addItem("");
 		comboSist.addItem("MANUTENÇÃO DOS SISTEMAS DE INCÊNDIO");
 		comboSist.addItem("Alarmes");
@@ -128,53 +128,53 @@ public class TelaIncendio extends JPanel {
 		
 		JLabel labelLocal = new JLabel("Local");
 		labelLocal.setFont(new Font("Arial", Font.BOLD, 14));
-		panel.add(labelLocal, "flowx,cell 0 18");
+		panel.add(labelLocal, "flowx,cell 0 17");
 		
 		JLabel labelAsterisco2 = new JLabel("*");
 		labelAsterisco2.setForeground(Color.RED);
 		labelAsterisco2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		panel.add(labelAsterisco2, "cell 0 18");
+		panel.add(labelAsterisco2, "cell 0 17");
 		
 		JLabel labelDetalhesLocal = new JLabel("Prédio/andar ou ponto de referência");
 		labelDetalhesLocal.setFont(new Font("Arial", Font.PLAIN, 11));
-		panel.add(labelDetalhesLocal, "cell 0 19");
+		panel.add(labelDetalhesLocal, "cell 0 18");
 		
 		fieldLocal = new JTextField();
 		fieldLocal.setColumns(10);
-		panel.add(fieldLocal, "cell 0 20,growx");
+		panel.add(fieldLocal, "cell 0 19,growx");
 		
 		JLabel labelFoneContato = new JLabel("Fone de contato");
 		labelFoneContato.setFont(new Font("Arial", Font.BOLD, 14));
-		panel.add(labelFoneContato, "flowx,cell 0 22");
+		panel.add(labelFoneContato, "flowx,cell 0 21");
 		
 		fieldContato = new JTextField();
 		fieldContato.setColumns(10);
-		panel.add(fieldContato, "cell 0 23,growx");
+		panel.add(fieldContato, "cell 0 22,growx");
 		
 		JLabel labelAsterisco3 = new JLabel("*");
 		labelAsterisco3.setForeground(Color.RED);
 		labelAsterisco3.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		panel.add(labelAsterisco3, "cell 0 22");
+		panel.add(labelAsterisco3, "cell 0 21");
 		
 		JLabel LabelObserv = new JLabel("Observações");
 		LabelObserv.setFont(new Font("Arial", Font.BOLD, 14));
-		panel.add(LabelObserv, "cell 0 25");
+		panel.add(LabelObserv, "cell 0 24");
 		
 		JLabel LabelObservDetalhes = new JLabel("<html>Relatar se a irregularidade está causando danos ao patrimônio ou pode causar risco à comunidade, bem como inserir demais informações relevantes como, por exemplo, se haverá algum evento no local<html>");
 		LabelObservDetalhes.setFont(new Font("Arial", Font.PLAIN, 11));
-		panel.add(LabelObservDetalhes, "cell 0 26");
+		panel.add(LabelObservDetalhes, "cell 0 25");
 		
 		 obsArea = new JTextArea();
 		obsArea.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 2, true), "Obs", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.add(obsArea, "cell 0 27,grow");
+		panel.add(obsArea, "cell 0 26,grow");
 		
 		 buttonEnviar = new JButton("Envio");
 		buttonEnviar.setFont(new Font("Arial", Font.BOLD, 12));
-		panel.add(buttonEnviar, "flowx,cell 0 29,alignx center,aligny center");
+		panel.add(buttonEnviar, "flowx,cell 0 28,alignx center,aligny center");
 		
 		 buttonCancelar = new JButton("Cancelar");
 		buttonCancelar.setFont(new Font("Arial", Font.BOLD, 12));
-		panel.add(buttonCancelar, "cell 0 29,alignx center,aligny center");
+		panel.add(buttonCancelar, "cell 0 28,alignx center,aligny center");
 		buttonCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			 if(e.getActionCommand().equals("Cancelar"))
